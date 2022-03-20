a67d001fa2510936ac931af02847a72d8589e933
Ashish <ashishmathew.77@gmail.com>
2022-03-19 20:04:38 -0500

    added template files for Modified Binary Search and Bitwise Xor

---
+++ src/com/BitXOR/Main.java
@@ -0,0 +1,11 @@
+package com.BitXOR;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("BitXOR");
+
+
+    }
+}
---
+++ src/com/ModifiedBinarySearch/Main.java
@@ -0,0 +1,11 @@
+package com.ModifiedBinarySearch;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("ModifiedBinarySearch");
+
+
+    }
+}
