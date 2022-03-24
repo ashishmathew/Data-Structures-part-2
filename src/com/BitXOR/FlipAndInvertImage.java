5c34bda711ae73d54abebb4d953513f22f130027
Ashish <ashishmathew.77@gmail.com>
2022-03-23 21:35:44 -0500

    Added new logic for KLargest numbers

--- src/com/BitXOR/FlipAndInvertImage.java
+++ src/com/BitXOR/FlipAndInvertImage.java
@@ -1,4 +1,5 @@
 package com.BitXOR;
+
 import java.util.*;
 
 public class FlipAndInvertImage {
@@ -7,19 +8,19 @@ public class FlipAndInvertImage {
         //TODO: Write your code here
         //[1 1 0 0]
         int C = arr[0].length;
-        for(int[] row : arr){
-            for (int i = 0; i < (C+1)/2; i++) {
+        for (int[] row : arr) {
+            for (int i = 0; i < (C + 1) / 2; i++) {
                 int tmp = row[i] ^ 1;//[1 1 0 0]   tmp  = 0
-                row[i] = row[C-1-i] ^ 1;//[1 1 0 0]
-                row[C-1-i] = tmp;//[1 1 0 0]
+                row[i] = row[C - 1 - i] ^ 1;//[1 1 0 0]
+                row[C - 1 - i] = tmp;//[1 1 0 0]
             }
         }
         return arr;
     }
 
     public static void print(int[][] arr) {
-        for(int i=0; i < arr.length; i++) {
-            for (int j=0; j < arr[i].length; j++) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();
@@ -30,7 +31,7 @@ public class FlipAndInvertImage {
         int[][] arr = {{1, 0, 1}, {1, 1, 1}, {0, 1, 1}};
         print(flipAndInvertImage(arr));
 
-        int[][]arr2 = {{1,1,0,0},{1,0,0,1},{0,1,1,1},{1,0,1,0}};
+        int[][] arr2 = {{1, 1, 0, 0}, {1, 0, 0, 1}, {0, 1, 1, 1}, {1, 0, 1, 0}};
         print(flipAndInvertImage(arr2));
     }
 }
---
+++ src/com/topKelements/KLargestNumbers.java
@@ -0,0 +1,41 @@
+package com.topKelements;
+import java.util.*;
+
+
+public class KLargestNumbers {
+    public static List<Integer> findKLargestNumbers(int[] nums, int k) {
+        // TODO: Write your code here
+        //3, 1, 5, 12, 2, 11
+        //Store the first K elements in the min heap
+        PriorityQueue<Integer> pq = new PriorityQueue<>((n1,n2)->n1-n2);
+        for (int i = 0; i < k; i++) { // K * Log k
+            pq.add(nums[i]);
+        }
+
+        //go through the array for the rest of the elements
+        //and compare it to the first element in the heap
+        //Remove the element from the heap if its smaller compared to t
+        for (int i = k; i < nums.length; i++) //{ N-k * logK
+            if(pq.peek() < nums[i]){
+                pq.poll();
+                pq.add(nums[i]);
+            }
+
+        }
+
+        return new ArrayList<>(pq);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> result = KLargestNumbers.findKLargestNumbers(new int[] { 3, 1, 5, 12, 2, 11 }, 3);
+        System.out.println("Here are the top K numbers: " + result);
+
+        result = KLargestNumbers.findKLargestNumbers(new int[] { 5, 12, 11, -1, 12 }, 3);
+        System.out.println("Here are the top K numbers: " + result);
+    }
+
+
+
+
+
+}
